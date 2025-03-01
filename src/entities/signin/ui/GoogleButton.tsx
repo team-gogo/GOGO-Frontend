4355616b7f0174a5cbf6e27423af9662c284265f
Ethen1264 <jxorhks86909763@gmail.com>
2025-03-01 21:31:49 +0900

    update: cn 함수 적용 & 변경된 폰트 적용

--- src/entities/signin/ui/GoogleButton.tsx
+++ src/entities/signin/ui/GoogleButton.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { GoogleLogo } from '@/shared/assets/svg';
+import { cn } from '@/shared/utils/cn';
 
 const GoogleButton = () => {
   return (
-    <button className="flex w-full items-center justify-between rounded-lg bg-white p-16 text-body2b text-black">
+    <button
+      className={cn(
+        'flex',
+        'w-full',
+        'items-center',
+        'justify-between',
+        'rounded-lg',
+        'bg-white',
+        'p-16',
+      )}
+    >
       <GoogleLogo />
-      <span className="flex-grow text-center text-body2m text-black">
+      <span
+        className={cn('text-body2s', 'flex-grow', 'text-center', 'text-black')}
+      >
         Google 계정으로 시작하기
       </span>
     </button>
--- src/views/signin/ui/SigninPage/index.tsx
+++ src/views/signin/ui/SigninPage/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { cn } from '@/shared/utils/cn';
 import { SigninContainer } from '@/widgets/signin';
 
 const SigninPage = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div
+      className={cn(
+        'flex',
+        'h-screen',
+        'w-screen',
+        'items-center',
+        'justify-center',
+      )}
+    >
       <SigninContainer />
     </div>
   );
