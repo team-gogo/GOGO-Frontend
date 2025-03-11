792bca678cd549d045d5cd976d6fbc8b8ac66151
gjaegyun <jaegyungim06@gmail.com>
2025-03-12 01:40:15 +0900

    feat: index.ts 파일 정리

--- src/views/my/bet/index.ts
+++ src/views/my/bet/index.ts
@@ -1,2 +1,3 @@
 export { default as MyBetPage } from './ui/MyBetPage';
 export { default as getUserBetInfo } from './ui/Mock/getUserBetInfo';
+export { default as getTempPoint } from './ui/Mock/getTempPoint';
--- src/views/my/index.ts
+++ src/views/my/index.ts
@@ -1,2 +1,4 @@
 export { default as MyPage } from './ui/MyPage';
 export { default as MyBetPage } from './bet/ui/MyBetPage';
+export { default as getUserStageInfo } from './ui/Mock/getUserStageInfo';
+export { default as getUserInfo } from './ui/Mock/getUserInfo';
