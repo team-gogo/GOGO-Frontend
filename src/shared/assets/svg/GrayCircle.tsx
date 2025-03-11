a30c492ba34cba754f028dc312b5029d82bd83f8
gjaegyun <jaegyungim06@gmail.com>
2025-03-11 09:15:13 +0900

    feat: icon 추가

---
+++ src/shared/assets/svg/GrayCircle.tsx
@@ -0,0 +1,20 @@
+const GrayCircle = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.5rem"
+    height="1.5rem"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="11"
+      fill="#6B6B6B"
+      stroke="#6B6B6B"
+      strokeWidth="2"
+    />
+  </svg>
+);
+
+export default GrayCircle;
