d224147dbed427d039cabb02e020c033a52bda5f
gjaegyun <jaegyungim06@gmail.com>
2025-03-13 00:58:40 +0900

    feat: icon 추가 및 props 추가

--- src/shared/assets/svg/index.ts
+++ src/shared/assets/svg/index.ts
@@ -30,3 +30,4 @@ export { default as GrayAlarmIcon } from './GrayAlarmIcon';
 export { default as BlueAlarmIcon } from './BlueAlarmIcon';
 export { default as RightArrowIcon } from './RightArrowIcon';
 export { default as SelectHeartIcon } from './SelectHeartIcon';
+export { default as CreateIcon } from './CreateIcon';
