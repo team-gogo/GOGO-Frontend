7a04be6c3e6b63cdb6594b0b18e60d47028a339c
Ethen1264 <jxorhks86909763@gmail.com>
2025-03-12 17:50:18 +0900

    fix: 랭킹이 반영되지 않는 이슈를 해결함

--- src/shared/assets/svg/RankBar.tsx
+++ src/shared/assets/svg/RankBar.tsx
@@ -4,36 +4,34 @@ interface RankBarProps {
   rankText: string;
 }
 
-const RankBar = ({ height, color, rankText }: RankBarProps) => {
+const RankBar: React.FC<RankBarProps> = ({ height, color, rankText }) => {
   const maxHeight = 175;
   const adjustedHeight = (height / 100) * maxHeight;
 
   return (
-    <div className="aspect-[123/175] w-full max-w-[123px]">
-      <svg
-        className="h-full w-full"
-        viewBox={`0 0 123 ${adjustedHeight}`}
-        preserveAspectRatio="none"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+    <svg
+      width="123"
+      height={adjustedHeight}
+      viewBox={`0 0 123 ${adjustedHeight}`}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d={`M0 12C0 5.37259 5.37258 0 12 0H111C117.627 0 123 5.37258 123 12V${adjustedHeight}H0V12Z`}
+        fill={color}
+      />
+      <text
+        x="61.5"
+        y={adjustedHeight / 2}
+        dominantBaseline="middle"
+        textAnchor="middle"
+        fill="white"
+        fontSize="24"
+        fontWeight="bold"
       >
-        <path
-          d={`M0 12C0 5.37259 5.37258 0 12 0H111C117.627 0 123 5.37258 123 12V${adjustedHeight}H0V12Z`}
-          fill={color}
-        />
-        <text
-          x="61.5"
-          y={adjustedHeight / 2}
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fill="white"
-          fontSize="24"
-          fontWeight="bold"
-        >
-          {rankText}
-        </text>
-      </svg>
-    </div>
+        {rankText}
+      </text>
+    </svg>
   );
 };
 
