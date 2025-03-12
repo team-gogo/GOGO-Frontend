f9225ff7c7c043ce0ffe434e7465e2c752f98705
gjaegyun <jaegyungim06@gmail.com>
2025-03-13 08:46:41 +0900

    feat: section wrapper 제작

--- src/views/main/ui/MainPage/index.tsx
+++ src/views/main/ui/MainPage/index.tsx
@@ -1,10 +1,14 @@
 import { DateContainer } from '@/entities/main';
 import { cn } from '@/shared/utils/cn';
+import { SectionWrapper } from '@/widgets/main';
 
 const MainPage = () => {
   return (
     <div className={cn('w-full', 'flex', 'justify-between')}>
       <DateContainer />
+      <SectionWrapper text={'미니게임'} icon={<>hello</>} path="/my">
+        hello2
+      </SectionWrapper>
     </div>
   );
 };
---
+++ src/widgets/main/index.ts
@@ -0,0 +1,1 @@
+export { default as SectionWrapper } from './ui/SectionWrapper';
