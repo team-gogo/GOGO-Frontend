5405edb5bb94dd4ae08905524e92d5e86da2a024
gjaegyun <jaegyungim06@gmail.com>
2025-03-13 01:00:02 +0900

    feat: stage type 추가 및 optional type 반영

--- src/shared/ui/stage/index.tsx
+++ src/shared/ui/stage/index.tsx
@@ -1,61 +1,78 @@
+import { Dispatch, SetStateAction } from 'react';
 import { MyStageType } from '@/shared/types/my';
+import { StagesType } from '@/shared/types/stage';
 import { cn } from '@/shared/utils/cn';
 import Button from '../button';
 import MatchTypeLabel from '../matchTypeLabel';
 
 interface StageProps {
-  stage: MyStageType;
+  stage: MyStageType | StagesType;
+  setIsModalOpen?: Dispatch<SetStateAction<boolean>>;
 }
 
-const Stage = ({ stage }: StageProps) => {
+const Stage = ({ stage, setIsModalOpen }: StageProps) => {
   const { stageName, type, status, isMaintaining } = stage;
 
+  const isParticipating =
+    'isParticipating' in stage ? stage.isParticipating : undefined;
+
+  const isPassCode = 'isPassCode' in stage ? stage.isPassCode : undefined;
+
   return (
-    <div
-      className={cn(
-        'flex',
-        'flex-col',
-        'p-[1.5rem]',
-        'px-[2rem]',
-        'rounded-xl',
-        'bg-gray-700',
-      )}
-    >
-      <div className={cn('flex', 'flex-col', 'justify-center', 'gap-[3rem]')}>
-        <div
-          className={cn('flex', 'w-full', 'justify-between', 'items-center')}
-        >
-          <div className={cn('flex', 'items-center', 'gap-[0.625rem]')}>
-            {type.map((t) => (
-              <MatchTypeLabel key={t} type={t} color="#FFF" />
-            ))}
-            {isMaintaining && <MatchTypeLabel type="ADMIN" color="#526FFE" />}
-            {status === 'RECRUITING' ? (
-              <MatchTypeLabel type="RECRUITING" color="#01C612" />
-            ) : status === 'CONFIRMED' ? (
-              <MatchTypeLabel type="CONFIRMED" color="#898989" />
-            ) : null}
+    <>
+      <div
+        className={cn(
+          'flex',
+          'flex-col',
+          'p-[1.5rem]',
+          'px-[2rem]',
+          'rounded-xl',
+          'bg-gray-700',
+          'max-w-[40rem]',
+          'w-full',
+        )}
+      >
+        <div className={cn('flex', 'flex-col', 'justify-center', 'gap-[3rem]')}>
+          <div
+            className={cn('flex', 'w-full', 'justify-between', 'items-center')}
+          >
+            <div className={cn('flex', 'items-center', 'gap-[0.625rem]')}>
+              {type.map((t) => (
+                <MatchTypeLabel key={t} type={t} color="#FFF" />
+              ))}
+              {isMaintaining && <MatchTypeLabel type="ADMIN" color="#526FFE" />}
+              {status === 'RECRUITING' ? (
+                <MatchTypeLabel type="RECRUITING" color="#01C612" />
+              ) : status === 'CONFIRMED' ? (
+                <MatchTypeLabel type="CONFIRMED" color="#898989" />
+              ) : null}
+            </div>
+            {/* {isAdmin && <Tag TagType={'STREAMING'} />} */}
           </div>
-          {/* {isAdmin && <Tag TagType={'STREAMING'} />} */}
-        </div>
-        <div
-          className={cn(
-            'flex',
-            'w-full',
-            'flex-col',
-            'items-center',
-            'gap-[3rem]',
-          )}
-        >
-          <h1 className={cn('text-h2e', 'text-white', 'laptop:text-body2e')}>
-            {stageName}
-          </h1>
-          <Button isLocked={isMaintaining ? false : true}>
-            {isMaintaining ? '상세보기' : '참여하기'}
-          </Button>
+          <div
+            className={cn(
+              'flex',
+              'w-full',
+              'flex-col',
+              'items-center',
+              'gap-[3rem]',
+            )}
+          >
+            <h1 className={cn('text-h2e', 'text-white', 'laptop:text-body2e')}>
+              {stageName}
+            </h1>
+            <Button
+              isLocked={isParticipating ? false : true}
+              onClick={() => {
+                isPassCode ? setIsModalOpen?.(true) : console.log(1);
+              }}
+            >
+              {isParticipating ? '상세보기' : '참여하기'}
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
