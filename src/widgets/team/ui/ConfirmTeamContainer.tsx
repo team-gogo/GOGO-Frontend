da1267883eeec066749931c34ec4a41327d30b43
송재욱 <haensol976520@gmail.com>
2025-03-14 07:51:43 +0900

    refector: SelectedTeamCounter 컴포넌트 분리

--- src/widgets/team/ui/ConfirmTeamContainer.tsx
+++ src/widgets/team/ui/ConfirmTeamContainer.tsx
@@ -1,55 +1,41 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { cn } from '@/shared/utils/cn';
 import BackPageButton from '@/shared/ui/backPageButton';
 import Button from '@/shared/ui/button';
 import TeamItem from '@/entities/team/ui/TeamItem';
 import { getTeamItemMock } from '../Mock/getTeamItemMock';
+import SelectedTeamCounter from '@/entities/team/ui/SelectedTeamCounter';
 
 const ConfirmTeamContainer = () => {
   const [selectedTeamIds, setSelectedTeamIds] = useState<number[]>([]);
   const teams = getTeamItemMock();
 
-  const handleViewDetails = (teamId: number) => {};
+  const handleViewDetails = useCallback((teamId: number) => {}, []);
 
-  const handleConfirmTeam = () => {};
+  const handleConfirmTeam = useCallback(() => {}, []);
 
-  const handleToggleSelect = (teamId: number) => {
+  const handleToggleSelect = useCallback((teamId: number) => {
     setSelectedTeamIds((prev) => {
       if (prev.includes(teamId)) {
         return prev.filter((id) => id !== teamId);
-      } else {
-        return [...prev, teamId];
       }
+      return [...prev, teamId];
     });
-  };
+  }, []);
 
   return (
     <div className={cn('min-h-screen', 'bg-black')}>
-      <div className={cn('pt-16', 'pb-20')}>
+      <header className={cn('pt-16', 'pb-20')}>
         <BackPageButton type="back" label="뒤로가기" />
-      </div>
+      </header>
 
       <div className={cn('flex', 'items-center', 'justify-between', 'mb-24')}>
         <h1 className={cn('text-h4s', 'text-gray-100')}>현재 등록된 팀들</h1>
         <div className={cn('flex', 'items-center', 'gap-8')}>
-          <div
-            className={cn(
-              'text-body3s',
-              'text-gray-400',
-              'flex',
-              'items-center',
-              'gap-3',
-              'mr-20',
-            )}
-          >
-            <span>선택된 팀 개수</span>
-            <span className="text-body2s text-gray-100">
-              {selectedTeamIds.length}
-            </span>
-          </div>
-          <div className="w-[200px]">
+          <SelectedTeamCounter count={selectedTeamIds.length} />
+          <div className={`w-[200px]`}>
             <Button
               onClick={handleConfirmTeam}
               className={cn(
@@ -59,7 +45,7 @@ const ConfirmTeamContainer = () => {
                 'justify-center',
                 'gap-2',
                 'bg-blue-600',
-                'h-[40px]',
+                `h-[$40px]`,
                 'transition-colors',
               )}
             >
