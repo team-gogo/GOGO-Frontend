7cb33fb2d658eac5ee50747c258438e831c21ece
gjaegyun <jaegyungim06@gmail.com>
2025-03-13 13:56:16 +0900

    feat: stage apply 컴포넌트 생성

---
+++ src/widgets/stage/apply/ui/StageApply/index.tsx
@@ -0,0 +1,70 @@
+import { MatchGameType } from '@/shared/types/stage/apply';
+import Button from '@/shared/ui/button';
+import MatchTypeLabel from '@/shared/ui/matchTypeLabel';
+import SportTypeLabel from '@/shared/ui/sportTypelabel';
+import { cn } from '@/shared/utils/cn';
+
+interface StageApplyProps {
+  game: MatchGameType;
+}
+
+const StageApply = ({ game }: StageApplyProps) => {
+  const { gameName, teamCount, category } = game;
+
+  return (
+    <div
+      className={cn(
+        'flex',
+        'flex-col',
+        'p-[1.5rem]',
+        'px-[2rem]',
+        'rounded-xl',
+        'bg-gray-700',
+        'max-w-[40rem]',
+        'w-full',
+        'h-full',
+      )}
+    >
+      <div
+        className={cn(
+          'flex',
+          'flex-col',
+          'justify-center',
+          'gap-[3rem]',
+          'flex-grow',
+        )}
+      >
+        <div
+          className={cn('flex', 'w-full', 'justify-between', 'items-center')}
+        >
+          <div className={cn('flex', 'items-center', 'gap-[0.625rem]')}>
+            <SportTypeLabel type={category[0]} />
+            <MatchTypeLabel
+              type={'TEAM'}
+              customText={String(teamCount)}
+              color="#01C612"
+            />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'flex',
+            'w-full',
+            'flex-col',
+            'items-center',
+            'gap-[3rem]',
+          )}
+        >
+          <h1 className={cn('text-body1e', 'text-white', 'laptop:text-body2e')}>
+            {gameName}
+          </h1>
+          <div className={cn('flex', 'w-full', 'justify-center')}>
+            <Button>신청하기</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StageApply;
