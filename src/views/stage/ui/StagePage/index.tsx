551598e1ac57e4dc8c4d44789960eca0baf304c9
gjaegyun <jaegyungim06@gmail.com>
2025-03-13 10:33:33 +0900

    fix: stage header 컴포넌트화 반영

--- src/views/stage/ui/StagePage/index.tsx
+++ src/views/stage/ui/StagePage/index.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import CreateButton from '@/shared/ui/createButton';
 import { cn } from '@/shared/utils/cn';
+import StageHeader from '@/widgets/stage/ui/StageHeader';
 import StageSection from '@/widgets/stage/ui/StageSection';
 import getStageInfo from '../Mock/getStageInfo';
 
 const StagePage = () => {
-  const { push } = useRouter();
   const stageInfo = getStageInfo();
 
   const participateStages = stageInfo.stages.filter(
@@ -28,17 +26,7 @@ const StagePage = () => {
     >
       <div className={cn('flex w-full max-w-[82.5rem] flex-col gap-[4rem]')}>
         <div className={cn('flex flex-col gap-[2.5rem]')}>
-          <div className={cn('flex w-full items-center justify-between')}>
-            <h1 className={cn('text-body1e text-white')}>참여하는 스테이지</h1>
-            <div className={cn('flex items-center gap-[1.5rem]')}>
-              <CreateButton onClick={() => push('/stage/create/fast')}>
-                빠른 경기 생성
-              </CreateButton>
-              <CreateButton onClick={() => push('/stage/create/official')}>
-                학교 공식 행사 생성
-              </CreateButton>
-            </div>
-          </div>
+          <StageHeader />
 
           <StageSection stages={participateStages} />
           <StageSection title="참여가능한 스테이지" stages={confirmedStages} />
