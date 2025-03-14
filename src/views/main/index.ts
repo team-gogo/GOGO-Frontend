a2cabf5f98a3e98854673a0da338e557e8c6751b
gjaegyun <jaegyungim06@gmail.com>
2025-03-14 12:09:38 +0900

    feat: mock data 생성

--- src/views/main/index.ts
+++ src/views/main/index.ts
@@ -1,2 +1,3 @@
 export { default as MainPage } from './ui/MainPage';
 export { default as getMatchInfo } from './ui/Mock/getMatchInfo';
+export { default as getRankingMock } from './ui/Mock/getRankingMock';
