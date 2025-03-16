244cc04c6904ee34553676edf7fe77b2d4a080ec
Ethen1264 <jxorhks86909763@gmail.com>
2025-03-16 11:19:11 +0900

    refector: minigame container

--- src/widgets/stage/create/official/ui/MiniGameContainer/index.tsx
+++ src/widgets/stage/create/official/ui/MiniGameContainer/index.tsx
@@ -5,16 +5,13 @@ import {
   UseFormWatch,
 } from 'react-hook-form';
 import { SelectStageType } from '@/entities/stage/create/official';
-import {
-  CoinIcon,
-  PlinkoIcon,
-  ShellGameIcon,
-  TicketIcon,
-} from '@/shared/assets/icons';
+import { TicketIcon } from '@/shared/assets/icons';
 import { PointIcon } from '@/shared/assets/svg';
 import { OfficialStageData } from '@/shared/types/stage/create/official';
 import Input from '@/shared/ui/input';
 import { cn } from '@/shared/utils/cn';
+import { MINI_GAMES } from '../../constants/miniGames';
+import { getIconColor, toggleMiniGame } from '../../model/miniGameUtils';
 
 interface Props {
   register: UseFormRegister<OfficialStageData>;
@@ -22,28 +19,10 @@ interface Props {
   setValue: UseFormSetValue<OfficialStageData>;
 }
 
-const miniGames = [
-  {
-    type: 'yavarwee' as const,
-    name: '야바위',
-    icon: <ShellGameIcon size={60} color="#898989" />,
-  },
-  {
-    type: 'coinToss' as const,
-    name: '코인 플립',
-    icon: <CoinIcon size={60} color="#898989" />,
-  },
-  {
-    type: 'plinko' as const,
-    name: '플링코',
-    icon: <PlinkoIcon size={60} color="#898989" />,
-  },
-];
-
 const MiniGameContainer = ({ register, watch, setValue }: Props) => {
   const selectedGames = watch('miniGame') || {};
 
-  miniGames.forEach((game) => {
+  MINI_GAMES.forEach((game) => {
     register(`miniGame.${game.type}.isActive`);
   });
 
@@ -51,26 +30,20 @@ const MiniGameContainer = ({ register, watch, setValue }: Props) => {
     <div className={cn('space-y-16')}>
       <p className={cn('text-body2e', 'text-white')}>미니게임</p>
       <div className={cn('flex', 'items-center', 'gap-24', 'tablet:flex-wrap')}>
-        {miniGames.map((game) => {
+        {MINI_GAMES.map((game) => {
           const isActive = selectedGames[game.type]?.isActive;
-          const iconColor = isActive ? '#526FFE' : '#898989';
+          const iconColor = getIconColor(isActive);
 
           return (
             <div key={game.type} className={cn('space-y-16', 'w-full')}>
               <SelectStageType
-                icon={cloneElement(game.icon, { color: iconColor })}
+                icon={cloneElement(<game.icon size={60} />, {
+                  color: iconColor,
+                })}
                 name={game.name}
                 isSelected={isActive}
-                onClick={() => {
-                  if (isActive) {
-                    setValue(`miniGame.${game.type}.maxBettingPoint`, null);
-                    setValue(`miniGame.${game.type}.minBettingPoint`, null);
-                    setValue(`miniGame.${game.type}.initialTicketCount`, null);
-                  }
-                  setValue(`miniGame.${game.type}.isActive`, !isActive);
-                }}
+                onClick={() => toggleMiniGame(game.type, isActive, setValue)}
               />
-
               <div className={cn('flex', 'gap-16')}>
                 <Input
                   {...register(`miniGame.${game.type}.maxBettingPoint`, {
