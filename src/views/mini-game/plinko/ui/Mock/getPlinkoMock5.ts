682d786b68e10fc597df626ae985a9aad61c9122
gjaegyun <jaegyungim06@gmail.com>
2025-03-18 15:01:43 +0900

    feat: index 별 목데이터 생성

---
+++ src/views/mini-game/plinko/ui/Mock/getPlinkoMock5.ts
@@ -0,0 +1,10 @@
+import { PlinkoResponse } from '@/shared/types/mini-game';
+
+const getPlinkoMock5 = (): PlinkoResponse => {
+  return {
+    amount: 5000,
+    multi: 5,
+  };
+};
+
+export default getPlinkoMock5;
---
+++ src/views/mini-game/plinko/ui/Mock/getPlinkoMock6.ts
@@ -0,0 +1,10 @@
+import { PlinkoResponse } from '@/shared/types/mini-game';
+
+const getPlinkoMock6 = (): PlinkoResponse => {
+  return {
+    amount: 5000,
+    multi: 6,
+  };
+};
+
+export default getPlinkoMock6;
