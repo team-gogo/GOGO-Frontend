2ec29338e8eb1dc92e08d5733bb56ac8c5eb37dc
gjaegyun <jaegyungim06@gmail.com>
2025-03-20 11:26:15 +0900

    fix: plinko test 주석 삭제

--- src/app/mini-game/plinko/page.tsx
+++ src/app/mini-game/plinko/page.tsx
@@ -1,10 +1,7 @@
 import { PlinkoPage } from '@/views/mini-game';
 
-// import PlinkoTest from '@/widgets/mini-game/plinko/ui/PlinkoTest';
-
 const page = () => {
   return <PlinkoPage />;
-  // return <PlinkoTest />;
 };
 
 export default page;
