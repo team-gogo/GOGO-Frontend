ac1cd24aae7077a0d0974367f2f5f31ddcee1d2e
gjaegyun <jaegyungim06@gmail.com>
2025-03-21 00:20:58 +0900

    feat: refetch 방지 state 적용

--- src/entities/main/ui/BatchModal/index.tsx
+++ src/entities/main/ui/BatchModal/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { WarningIcon } from '@/shared/assets/svg';
+import { useMatchBatchArrStore, useMatchTeamStore } from '@/shared/stores';
 import Button from '@/shared/ui/button';
 import Input from '@/shared/ui/input';
 import ModalLayout from '@/shared/ui/modalLayout';
@@ -17,12 +19,26 @@ interface BatchMatchType {
 }
 
 const BatchModal = ({ onClose }: BatchModalProps) => {
-  const { register, handleSubmit, reset } = useForm<BatchMatchType>();
+  const { aTeam, bTeam, matchId } = useMatchTeamStore();
+  const { matchBatchArr, setMatchBatchArr } = useMatchBatchArrStore();
+
+  const { register, handleSubmit, reset, watch } = useForm<BatchMatchType>();
+
+  const { aTeamScore, bTeamScore } = watch();
+
+  const isDisabled = !aTeamScore || !bTeamScore;
 
   const onSubmit: SubmitHandler<BatchMatchType> = async (data) => {
     console.log(data);
 
+    const updatedMatchBatchArr = matchBatchArr.map((item) =>
+      item.matchId === matchId ? { ...item, isEnd: true } : item,
+    );
+
+    setMatchBatchArr(updatedMatchBatchArr);
+
     reset();
+    onClose();
   };
 
   return (
@@ -57,6 +73,7 @@ const BatchModal = ({ onClose }: BatchModalProps) => {
             'justify-center',
             'gap-[1.5rem]',
             'w-full',
+            'pt-[2rem]',
           )}
         >
           <div
@@ -68,7 +85,9 @@ const BatchModal = ({ onClose }: BatchModalProps) => {
               'gap-[1rem]',
             )}
           >
-            <h2 className={cn('text-h4e', 'text-white')}>A팀</h2>
+            <h2 className={cn('text-h4e', 'text-white')}>
+              {aTeam?.teamName}팀
+            </h2>
             <Input
               {...register('aTeamScore', { required: true })}
               placeholder="스코어 입력"
@@ -86,7 +105,9 @@ const BatchModal = ({ onClose }: BatchModalProps) => {
               'gap-[1rem]',
             )}
           >
-            <h2 className={cn('text-h4e', 'text-white')}>B팀</h2>
+            <h2 className={cn('text-h4e', 'text-white')}>
+              {bTeam?.teamName}팀
+            </h2>
             <Input
               {...register('bTeamScore', { required: true })}
               placeholder="스코어 입력"
@@ -95,7 +116,25 @@ const BatchModal = ({ onClose }: BatchModalProps) => {
             />
           </div>
         </div>
-        <Button type="submit">정산완료</Button>
+        <div
+          className={cn(
+            'flex',
+            'w-full',
+            'flex-col',
+            'items-center',
+            'gap-[0.75rem]',
+          )}
+        >
+          <Button disabled={isDisabled} type="submit">
+            정산완료
+          </Button>
+          <div className={cn('flex', 'items-center', 'gap-[0.5rem]')}>
+            <WarningIcon />
+            <p className={cn('text-caption1s', 'text-gray-500')}>
+              정산 완료 후 5분 이내에만 취소할 수 있습니다.
+            </p>
+          </div>
+        </div>
       </form>
     </ModalLayout>
   );
