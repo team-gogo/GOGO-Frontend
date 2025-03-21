2c7a6961c532436fabf7783ce02b498e36324866
송재욱 <haensol976520@gmail.com>
2025-03-22 00:58:34 +0900

    feat: 안내 메시지 추가' ;

--- src/widgets/team/ui/CreateTeamContainer/index.tsx
+++ src/widgets/team/ui/CreateTeamContainer/index.tsx
@@ -4,6 +4,7 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import SearchIcon from '@/shared/assets/icons/SearchIcon';
+import ExclamationIcon from '@/shared/assets/svg/ExclamationIcon';
 import BackPageButton from '@/shared/ui/backPageButton';
 import Button from '@/shared/ui/button';
 import Input from '@/shared/ui/input';
@@ -58,7 +59,23 @@ const CreateTeamContainer = () => {
           </div>
         </div>
         <div>
-          <h2 className={cn('text-body2e', 'text-white', 'mt-24')}>인원</h2>
+          <div className={cn('flex', 'justify-between', 'align-bottom')}>
+            <h2 className={cn('text-body2e', 'text-white', 'mt-24')}>인원</h2>
+            <h4
+              className={cn(
+                'text-body3s',
+                'text-gray-500',
+                'mt-24',
+                'flex',
+                'justify-between',
+                'gap-16',
+              )}
+            >
+              <ExclamationIcon />
+              <span>최소 인원 : 0명</span>
+              <span>최대 인원 : 4명</span>
+            </h4>
+          </div>
           <div className={cn('mt-24')}>
             <Input
               placeholder="학생을 입력해주세요."
