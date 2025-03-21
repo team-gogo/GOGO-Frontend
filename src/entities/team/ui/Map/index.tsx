6186ffbdacab73f96c621d14e0f40cffdbb73e0f
송재욱 <haensol976520@gmail.com>
2025-03-21 10:35:50 +0900

    feat: 배드민턴, 농구에 대해 기능 확장

--- src/entities/team/ui/Map/index.tsx
+++ src/entities/team/ui/Map/index.tsx
@@ -1,4 +1,6 @@
 // import { SportType } from '@/shared/model/sportTypes';
+import BadmintonMap from './Badminton';
+import BasketballMap from './Basketball';
 import DefaultMap from './Default';
 import { SportMapProps } from './types';
 import VolleyballMap from './Volleyball';
@@ -7,6 +9,10 @@ const SportMap = ({ type, onPlayerDrag, players }: SportMapProps) => {
   switch (type) {
     case 'VOLLEY_BALL':
       return <VolleyballMap onPlayerDrag={onPlayerDrag} players={players} />;
+    case 'BASKET_BALL':
+      return <BasketballMap onPlayerDrag={onPlayerDrag} players={players} />;
+    case 'BADMINTON':
+      return <BadmintonMap onPlayerDrag={onPlayerDrag} players={players} />;
     default:
       return <DefaultMap onPlayerDrag={onPlayerDrag} players={players} />;
   }
