24180085983e4161c079569a0b1cb8ab0ab06586
gjaegyun <jaegyungim06@gmail.com>
2025-03-21 09:27:34 +0900

    feat: useBettingForm.ts 제작

---
+++ src/entities/main/model/useBettingForm.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useForm, FieldErrors } from 'react-hook-form';
+import { BettingFormData } from '@/shared/types/main';
+
+export const useBettingForm = () => {
+  const { register, handleSubmit, setValue, watch } =
+    useForm<BettingFormData>();
+  const [selectedTeamId, setSelectedTeamId] = useState<number | null>(null);
+
+  const bettingPoint = watch('bettingPoint');
+
+  const isDisabled = !bettingPoint || !selectedTeamId;
+
+  const onError = (errors: FieldErrors<BettingFormData>) => {
+    console.log('제출에러:', errors);
+  };
+
+  return {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    selectedTeamId,
+    setSelectedTeamId,
+    isDisabled,
+    onError,
+  };
+};
