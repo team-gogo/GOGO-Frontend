1c87c5e00353b2211248d6d93406d128e9f856f8
송재욱 <haensol976520@gmail.com>
2025-03-21 10:27:34 +0900

    feat: add svg

---
+++ src/shared/assets/svg/Map/Badminton.tsx
@@ -0,0 +1,21 @@
+const Badminton = () => {
+  return (
+    <svg
+      width="536"
+      height="488"
+      viewBox="0 0 536 488"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="536" height="488" rx="12" fill="#303030" />
+      <path
+        d="M34 59C34 53.4771 38.4772 49 44 49H91.4541H449.723H491C496.523 49 501 53.4771 501 59V188.389V413.853V443C501 448.523 496.523 453 491 453H449.723H270.589H91.4541H44C38.4772 453 34 448.523 34 443V413.853V188.389V59Z"
+        stroke="#4D4D4D"
+        strokeWidth="4"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Badminton;
