ca9e47a5f4d7c6f00347875ba50375b439f3d747
gjaegyun <jaegyungim06@gmail.com>
2025-03-24 20:10:53 +0900

    feat: postPassCode.ts 제작

---
+++ src/entities/stage/ui/model/usePostPassCode.ts
@@ -0,0 +1,19 @@
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { PassCodeType } from '@/shared/types/stage';
+import { postPassCode } from '../api/postPassCode';
+
+export const usePostPassCode = (stageId: number) => {
+  const { push } = useRouter();
+  return useMutation({
+    mutationFn: (data: PassCodeType) => postPassCode(stageId, data),
+    onSuccess: () => {
+      toast.success('스테이지 참여가 완료되었습니다.');
+      push(`/stage/${stageId}`);
+    },
+    onError: (error: Error) => {
+      toast.error(error.message);
+    },
+  });
+};
