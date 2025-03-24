908eca2b5382fd94a96d19803a44db44874c49d5
송재욱 <haensol976520@gmail.com>
2025-03-24 22:52:29 +0900

    fix: inviteStudentInput 재사용하도록 수정

--- src/widgets/team/ui/CreateTeamContainer/index.tsx
+++ src/widgets/team/ui/CreateTeamContainer/index.tsx
@@ -2,21 +2,27 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
-import SearchIcon from '@/shared/assets/icons/SearchIcon';
 import ExclamationIcon from '@/shared/assets/svg/ExclamationIcon';
+import { StageData } from '@/shared/types/stage/create';
 import BackPageButton from '@/shared/ui/backPageButton';
 import Button from '@/shared/ui/button';
 import Input from '@/shared/ui/input';
+import InviteStudentInput from '@/shared/ui/InviteStudentInput';
 import { cn } from '@/shared/utils/cn';
 
 const CreateTeamContainer = () => {
   const [teamName, setTeamName] = useState('');
-  const [members, setMembers] = useState('');
   const router = useRouter();
   const searchParams = useSearchParams();
+  const {
+    register,
+    setValue,
+    handleSubmit: handleFormSubmit,
+  } = useForm<StageData>();
 
-  const handleSubmit = () => {
+  const onSubmit = (data: StageData) => {
     if (
       !teamName.trim() ||
       teamName.trim().length > 10 ||
@@ -26,9 +32,9 @@ const CreateTeamContainer = () => {
       return;
     }
 
-    const membersList = members.split(',').map((member) => member.trim());
+    const membersList = data.maintainer || [];
 
-    if (!members.trim() || membersList.some((member) => !member)) {
+    if (membersList.length === 0) {
       toast.error('인원을 올바르게 입력해주세요.');
       return;
     }
@@ -45,7 +51,10 @@ const CreateTeamContainer = () => {
       <header className={cn('mb-30')}>
         <BackPageButton type="back" label="팀 생성하기" />
       </header>
-      <div className={cn('flex-1', 'flex', 'flex-col', 'mt-28', 'gap-28')}>
+      <form
+        onSubmit={handleFormSubmit(onSubmit)}
+        className={cn('flex-1', 'flex', 'flex-col', 'mt-28', 'gap-28')}
+      >
         <h1 className={cn('text-h3e', 'text-white', 'mb-28')}>경기 이름</h1>
         <div>
           <h2 className={cn('text-body2e', 'text-white', 'mt-24')}>팀 이름</h2>
@@ -77,20 +86,20 @@ const CreateTeamContainer = () => {
             </h4>
           </div>
           <div className={cn('mt-24')}>
-            <Input
-              placeholder="학생을 입력해주세요."
-              value={members}
-              onChange={(e) => setMembers(e.target.value)}
-              icon={<SearchIcon size={30} />}
+            <InviteStudentInput
+              register={register}
+              setValue={setValue}
+              title=" "
+              description=" "
             />
           </div>
         </div>
-      </div>
-      <div className={cn('mt-60', 'mb-30')}>
-        <Button bg="bg-main-600" textColor="text-white" onClick={handleSubmit}>
-          확인
-        </Button>
-      </div>
+        <div className={cn('mt-60', 'mb-30')}>
+          <Button type="submit" bg="bg-main-600" textColor="text-white">
+            확인
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
