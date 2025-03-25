d73f370bddfa07557c3e791a271043d34f3dd6d0
gjaegyun <jaegyungim06@gmail.com>
2025-03-25 14:14:43 +0900

    feat: icon 반응형 여부 props 생성

--- src/shared/assets/svg/TagIcon/OfficialIcon.tsx
+++ src/shared/assets/svg/TagIcon/OfficialIcon.tsx
@@ -1,7 +1,11 @@
-const OfficialIcon = () => (
+const OfficialIcon = ({ isResponsive = true }: { isResponsive?: boolean }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="h-[1.25rem] w-[1.25rem] laptop:h-[0.75rem] laptop:w-[0.75rem]"
+    className={
+      isResponsive
+        ? 'h-[1.25rem] w-[1.25rem] laptop:h-[0.75rem] laptop:w-[0.75rem]'
+        : 'h-[1.25rem] w-[1.25rem]'
+    }
     viewBox="0 0 12 12"
     fill="none"
   >
