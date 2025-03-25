a2318df8d87d2b89b7957feb4fe036fb53a909c2
gjaegyun <jaegyungim06@gmail.com>
2025-03-25 14:31:46 +0900

    feat: matchListSection 컴포넌트 제작

---
+++ src/widgets/main/ui/MatchListSection/index.tsx
@@ -0,0 +1,48 @@
+import { RightArrowIcon } from '@/shared/assets/svg';
+import { StageInMatchResponse } from '@/shared/types/main';
+import SportTypeLabel from '@/shared/ui/sportTypelabel';
+import { cn } from '@/shared/utils/cn';
+
+interface MatchListSectionProps {
+  stageInMatch: StageInMatchResponse;
+}
+
+const MatchListSection = ({ stageInMatch }: MatchListSectionProps) => {
+  return (
+    <div className={cn('flex', 'w-full', 'gap-[1rem]', 'flex-col')}>
+      {stageInMatch.games.slice(0, 4).map((game) => (
+        <div
+          key={game.gameId}
+          className={cn(
+            'w-full',
+            'flex',
+            'flex-col',
+            'px-[1rem]',
+            'py-[0.75rem]',
+            'rounded-xl',
+            'bg-gray-700',
+          )}
+        >
+          <div
+            className={cn('flex', 'justify-between', 'w-full', 'items-center')}
+          >
+            <SportTypeLabel
+              type={game.category}
+              isHaveBorder={true}
+              isMainUsed={true}
+            />
+            <p className={cn('text-body3s', 'text-white')}>{game.gameName}</p>
+            <button className={cn('flex', 'gap-[0.5rem]', 'items-center')}>
+              <p className={cn('text-caption1s', 'text-gray-300')}>
+                자세히보기
+              </p>
+              <RightArrowIcon size="1.25rem" />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MatchListSection;
