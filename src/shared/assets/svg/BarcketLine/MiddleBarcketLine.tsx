8829585a82a2faa2dfa38888fceba62f6bd5449e
송재욱 <haensol976520@gmail.com>
2025-03-27 00:16:07 +0900

    feat: add barcketline svg

---
+++ src/shared/assets/svg/BarcketLine/MiddleBarcketLine.tsx
@@ -0,0 +1,15 @@
+const MiddleBarcketLine = () => {
+  return (
+    <svg
+      width="345"
+      height="4"
+      viewBox="0 0 345 4"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 2H345" stroke="#4D4D4D" strokeWidth="4" />
+    </svg>
+  );
+};
+
+export default MiddleBarcketLine;
