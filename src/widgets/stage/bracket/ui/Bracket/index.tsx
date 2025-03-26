f510740bb31bd6eac501649e6657225f87171bcb
송재욱 <haensol976520@gmail.com>
2025-03-27 00:22:34 +0900

    feat: 4강 대진표 퍼블리싱

--- src/widgets/stage/bracket/ui/Bracket/index.tsx
+++ src/widgets/stage/bracket/ui/Bracket/index.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import LeftBracketLine from '@/shared/assets/svg/BarcketLine/LeftBarcketLine';
+import MiddleBracketLine from '@/shared/assets/svg/BarcketLine/MiddleBarcketLine';
+import RightBracketLine from '@/shared/assets/svg/BarcketLine/RightBarcketLine';
 import { cn } from '@/shared/utils/cn';
 
 const Bracket = () => {
@@ -10,8 +13,21 @@ const Bracket = () => {
       <header className={cn('mb-30')}>
         <h1 className={cn('text-h3e', 'text-white')}>{finalStage}강</h1>
       </header>
-      <div className={cn('h-[100%]', 'bg-gray-700', 'rounded-lg')}>
-        {/*  */}
+      <div className={cn('h-[100%]', 'bg-gray-700', 'rounded-lg', 'p-20')}>
+        <div
+          className={cn(
+            'w-full',
+            'h-full',
+            'flex',
+            'items-center',
+            'justify-center',
+            'gap-20',
+          )}
+        >
+          <LeftBracketLine />
+          <MiddleBracketLine />
+          <RightBracketLine />
+        </div>
       </div>
     </div>
   );
