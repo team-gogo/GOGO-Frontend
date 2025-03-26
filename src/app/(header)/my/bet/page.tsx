39ed0c3d225872ccb32a5fc1fd012348d6e77f79
gjaegyun <jaegyungim06@gmail.com>
2025-03-27 00:14:11 +0900

    fix: build error

--- src/app/(header)/my/bet/page.tsx
+++ src/app/(header)/my/bet/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import { MyBetPage } from '@/views/my';
 
 const page = () => {
-  return <MyBetPage />;
+  return (
+    <Suspense>
+      <MyBetPage />
+    </Suspense>
+  );
 };
 
 export default page;
