6d757025e3b3c3f6c61c1d670931c88e7f4ff0c8
gjaegyun <jaegyungim06@gmail.com>
2025-03-28 09:57:22 +0900

    feat: stale, gctime 추가

--- src/views/mini-game/model/useGetShopTicketStatusQuery.ts
+++ src/views/mini-game/model/useGetShopTicketStatusQuery.ts
@@ -1,10 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import { ShopTicketStatusDto } from '@/shared/types/mini-game';
+import minutesToMs from '@/shared/utils/minutesToms';
 import { getShopTicketStatus } from '../api/getShopTicketStatus';
 
 export const useGetShopTicketStatusQuery = (stageId: string) => {
   return useQuery<ShopTicketStatusDto, Error>({
     queryKey: ['shopTicketStatus', stageId],
     queryFn: () => getShopTicketStatus(stageId),
+    staleTime: minutesToMs(5),
+    gcTime: minutesToMs(5),
   });
 };
