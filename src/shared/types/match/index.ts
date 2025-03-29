de49ca397a0618930f4260f66c2f081faf3d4220
gjaegyun <jaegyungim06@gmail.com>
2025-03-30 03:44:05 +0900

    feat: teamDetailType 추가

--- src/shared/types/match/index.ts
+++ src/shared/types/match/index.ts
@@ -9,3 +9,16 @@ export type TeamListResponse = {
   count: number;
   team: Team[];
 };
+
+interface Participant {
+  studentId: number;
+  name: string;
+  classNumber: number;
+  studentNumber: number;
+  positionX: string;
+  positionY: string;
+}
+
+export interface TeamDetailType extends Team {
+  participant: Participant[];
+}
