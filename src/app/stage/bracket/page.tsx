e4f04408c5f6a815108347b74d293ed8b63c2c73
송재욱 <haensol976520@gmail.com>
2025-03-31 03:59:52 +0900

    feat: add Suspense

--- src/app/stage/bracket/page.tsx
+++ src/app/stage/bracket/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import { CreateBracketPage } from '@/views/stage/bracket';
 
 const page = () => {
-  return <CreateBracketPage />;
+  return (
+    <Suspense fallback={<div>로딩중...</div>}>
+      <CreateBracketPage />
+    </Suspense>
+  );
 };
 
 export default page;
--- src/app/stage/time/page.tsx
+++ src/app/stage/time/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
 import { SetTimePage } from '@/views/stage/time';
 
 const page = () => {
-  return <SetTimePage />;
+  return (
+    <Suspense fallback={<div>로딩중...</div>}>
+      <SetTimePage />
+    </Suspense>
+  );
 };
 
 export default page;
