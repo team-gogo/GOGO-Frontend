9744f00082574c99dbb9f1fa245bfcb484270695
gjaegyun <jaegyungim06@gmail.com>
2025-03-31 08:32:18 +0900

    feat: GOGOIcon 최신화

---
+++ src/shared/ui/GOGOIcon/index.tsx
@@ -0,0 +1,22 @@
+import { GIcon, OIcon } from '@/shared/assets/svg';
+import { cn } from '@/shared/utils/cn';
+
+interface SizeProps {
+  GWidth: string;
+  GHeight: string;
+  OWidth: string;
+  OHeight: string;
+}
+
+const GOGOIcon = ({ GWidth, GHeight, OWidth, OHeight }: SizeProps) => {
+  return (
+    <div className={cn('flex', 'gap-[0.0625rem]')}>
+      <GIcon GWidth={GWidth} GHeight={GHeight} />
+      <OIcon OWidth={OWidth} OHeight={OHeight} />
+      <GIcon GWidth={GWidth} GHeight={GHeight} />
+      <OIcon OWidth={OWidth} OHeight={OHeight} />
+    </div>
+  );
+};
+
+export default GOGOIcon;
--- src/widgets/signin/ui/SigninContainer.tsx
+++ src/widgets/signin/ui/SigninContainer.tsx
@@ -1,12 +1,17 @@
 import { GoogleButton } from '@/entities/signin';
-import { Logo } from '@/shared/assets/svg';
+import GOGOIcon from '@/shared/ui/GOGOIcon';
 import { cn } from '@/shared/utils/cn';
 
 const SigninContainer = () => {
   return (
     <div className={cn('w-full', 'max-w-[648px]', 'space-y-[100px]')}>
       <div className={cn('flex', 'justify-center')}>
-        <Logo width={420} height={144} />
+        <GOGOIcon
+          GWidth="8.12106rem"
+          GHeight="8.3795rem"
+          OWidth="8.695rem"
+          OHeight="8.56438rem"
+        />
       </div>
       <GoogleButton />
     </div>
