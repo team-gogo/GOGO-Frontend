35e26dbaca28223f1baf90b53381c795d11373b2
송재욱 <haensol976520@gmail.com>
2025-03-30 17:21:53 +0900

    feat: 요소크기조정

--- src/entities/stage/time/ui/MatchItem/index.tsx
+++ src/entities/stage/time/ui/MatchItem/index.tsx
@@ -8,12 +8,12 @@ interface MatchItemProps {
 
 const MatchItem = ({ index, teamA, teamB }: MatchItemProps) => {
   return (
-    <div className="flex min-h-20 w-[270px] items-center rounded-lg bg-gray-600 p-[8px_16px] text-white">
-      <div className="mr-[20px] text-[24px] font-bold">{index}</div>
+    <div className="flex min-h-20 w-[300px] items-center rounded-lg bg-gray-600 p-[8px_16px] text-white">
+      <div className="mr-[20px] text-h4s font-bold">{index}</div>
       <div className="flex flex-1 items-center justify-center">
-        <div className="text-[18px] font-medium">{teamA}</div>
-        <div className="mx-[10px] text-[16px] opacity-70">vs</div>
-        <div className="text-[18px] font-medium">{teamB}</div>
+        <div className="text-16 font-medium">{teamA}</div>
+        <div className="text-12 mx-[10px] opacity-70">vs</div>
+        <div className="text-16 font-medium">{teamB}</div>
       </div>
     </div>
   );
