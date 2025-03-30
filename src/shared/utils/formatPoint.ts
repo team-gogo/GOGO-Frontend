36dad49b3eaa2cb00b715fbbd56c2e542462aa44
gjaegyun <jaegyungim06@gmail.com>
2025-03-30 23:42:10 +0900

    fix: format 단위 수정

--- src/shared/utils/formatPoint.ts
+++ src/shared/utils/formatPoint.ts
@@ -1,8 +1,6 @@
 export const formatPoint = (point: number) => {
   if (point >= 100000000) {
     return `${Math.floor(point / 10000000) / 10}억P`;
-  } else if (point >= 1000000) {
-    return `${Math.floor(point / 100000) / 10}백만P`;
   } else if (point >= 10000) {
     return `${Math.floor(point / 1000) / 10}만P`;
   }
