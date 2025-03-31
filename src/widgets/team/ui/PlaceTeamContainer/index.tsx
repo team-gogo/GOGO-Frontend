8de06a7f2ec2407bfa6e240c5be7937baa4335fb
송재욱 <haensol976520@gmail.com>
2025-03-31 21:02:51 +0900

    de: console.log 제거

--- src/widgets/team/ui/PlaceTeamContainer/index.tsx
+++ src/widgets/team/ui/PlaceTeamContainer/index.tsx
@@ -60,8 +60,6 @@ const PlaceTeamContainer = ({ params }: PlaceTeamContainerProps) => {
 
   const { gameId, category } = params;
 
-  console.log(category);
-
   const getSportType = (): SportType | null => {
     if (
       category === 'BASKET_BALL' ||
