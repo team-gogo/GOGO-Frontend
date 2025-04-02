72d88f3861b15b5c21e6355318b58ff92406974b
gjaegyun <jaegyungim06@gmail.com>
2025-04-02 14:09:27 +0900

    fix: point betting 낙관적 업데이트

--- src/entities/main/model/usePostBettingMatch.ts
+++ src/entities/main/model/usePostBettingMatch.ts
@@ -1,17 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
-import { useBettingMatchArrStore } from '@/shared/stores';
+import { useBettingMatchArrStore, usePointStore } from '@/shared/stores';
 import { BettingFormData } from '@/shared/types/main';
 import { postBettingMatch } from '../api/postBettingMatch';
 
 export const usePostBettingMatch = (matchId: number, bettingPoint: number) => {
   const { setBettingMatchArr } = useBettingMatchArrStore();
+  const { point, setPoint } = usePointStore();
 
   return useMutation({
     mutationFn: (data: BettingFormData) => postBettingMatch(matchId, data),
     onSuccess: () => {
       toast.success('베팅이 완료되었습니다.');
       setBettingMatchArr([{ matchId, bettingPoint: bettingPoint }]);
+      setPoint(point - bettingPoint);
     },
     onError: (error: Error) => {
       toast.error(error.message);
