b6ad5d02743e6e8530ed6603287036ed38a5a9b8
송재욱 <haensol976520@gmail.com>
2025-04-05 14:24:56 +0900

    feat: mock 수정정

--- src/views/stage/bracket/Mock/getBracketMock.ts
+++ src/views/stage/bracket/Mock/getBracketMock.ts
@@ -10,53 +10,130 @@ interface BracketData {
   turn: number;
 }
 
-const getBracketMock = () => {
-  const BracketMockData: BracketData[] = [
-    {
-      teamAId: 1,
-      teamBId: 2,
-      round: 'QUARTER_FINALS',
-      turn: 1,
-    },
-    {
-      teamAId: 3,
-      teamBId: 4,
-      round: 'QUARTER_FINALS',
-      turn: 2,
-    },
-    {
-      teamAId: 5,
-      teamBId: 6,
-      round: 'QUARTER_FINALS',
-      turn: 3,
-    },
-    {
-      teamAId: 7,
-      teamBId: 8,
-      round: 'QUARTER_FINALS',
-      turn: 4,
-    },
-    {
-      teamAId: null,
-      teamBId: null,
-      round: 'SEMI_FINALS',
-      turn: 1,
-    },
-    {
-      teamAId: null,
-      teamBId: null,
-      round: 'SEMI_FINALS',
-      turn: 2,
-    },
-    {
-      teamAId: null,
-      teamBId: null,
-      round: 'FINALS',
-      turn: 1,
-    },
-  ];
+const getBracketMock = (teamCount: number = 8): BracketData[] => {
+  if (teamCount === 8) {
+    return [
+      {
+        teamAId: 1,
+        teamBId: 2,
+        round: 'QUARTER_FINALS',
+        turn: 1,
+      },
+      {
+        teamAId: 3,
+        teamBId: 4,
+        round: 'QUARTER_FINALS',
+        turn: 2,
+      },
+      {
+        teamAId: 5,
+        teamBId: 6,
+        round: 'QUARTER_FINALS',
+        turn: 3,
+      },
+      {
+        teamAId: 7,
+        teamBId: 8,
+        round: 'QUARTER_FINALS',
+        turn: 4,
+      },
+      {
+        teamAId: null,
+        teamBId: null,
+        round: 'SEMI_FINALS',
+        turn: 1,
+      },
+      {
+        teamAId: null,
+        teamBId: null,
+        round: 'SEMI_FINALS',
+        turn: 2,
+      },
+      {
+        teamAId: null,
+        teamBId: null,
+        round: 'FINALS',
+        turn: 1,
+      },
+    ];
+  }
 
-  return BracketMockData;
+  if (teamCount === 5) {
+    return [
+      {
+        teamAId: 1,
+        teamBId: 2,
+        round: 'QUARTER_FINALS',
+        turn: 1,
+      },
+      {
+        teamAId: null,
+        teamBId: 3,
+        round: 'SEMI_FINALS',
+        turn: 1,
+      },
+      {
+        teamAId: 4,
+        teamBId: 5,
+        round: 'SEMI_FINALS',
+        turn: 2,
+      },
+      {
+        teamAId: null,
+        teamBId: null,
+        round: 'FINALS',
+        turn: 1,
+      },
+    ];
+  }
+
+  if (teamCount === 4) {
+    return [
+      {
+        teamAId: 1,
+        teamBId: 2,
+        round: 'SEMI_FINALS',
+        turn: 1,
+      },
+      {
+        teamAId: 3,
+        teamBId: 4,
+        round: 'SEMI_FINALS',
+        turn: 2,
+      },
+      {
+        teamAId: null,
+        teamBId: null,
+        round: 'FINALS',
+        turn: 1,
+      },
+    ];
+  }
+
+  if (teamCount === 3) {
+    return [
+      {
+        teamAId: 1,
+        teamBId: 2,
+        round: 'SEMI_FINALS',
+        turn: 1,
+      },
+      {
+        teamAId: 3,
+        teamBId: null,
+        round: 'SEMI_FINALS',
+        turn: 2,
+      },
+      {
+        teamAId: null,
+        teamBId: null,
+        round: 'FINALS',
+        turn: 1,
+      },
+    ];
+  }
+
+  return [];
 };
 
 export default getBracketMock;
