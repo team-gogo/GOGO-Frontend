351eb52a120b651059248df3a039826cebf54a01
송재욱 <haensol976520@gmail.com>
2025-04-05 15:56:44 +0900

    feat: 대진표 보기 버튼 제작

--- src/views/match/team/ui/MatchTeamPage/index.tsx
+++ src/views/match/team/ui/MatchTeamPage/index.tsx
@@ -3,23 +3,26 @@
 import { useParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { useGetStageGameQuery } from '@/entities/community/model/useGetStageGameQuery';
+import ShowBracketButton from '@/entities/match/team/ui/ShowBracketButton';
+import BracketModal from '@/entities/stage/ui/BracketModal';
 import {
   useSelectedGameIdStore,
   useTeamDetailInfoStore,
   useTeamDetailModalStore,
 } from '@/shared/stores';
+import useBracketModalStore from '@/shared/stores/useBracketModalStore';
 import BackPageButton from '@/shared/ui/backPageButton';
 import TeamDetailModal from '@/shared/ui/teamDetailModal';
 import { cn } from '@/shared/utils/cn';
 import { useGetTeamInfo } from '@/views/match/model/useGetTeamInfo';
 import { MatchNameContainer, TeamListContainer } from '@/widgets/match';
-
 const MatchTeamPage = () => {
   const params = useParams<{ stageId: string }>();
   const { stageId } = params;
 
   const { data: gameData } = useGetStageGameQuery(stageId);
 
+  const { isBracketModalOpen, setIsBracketModalOpen } = useBracketModalStore();
   const { selectedGameId, setSelectedGameId } = useSelectedGameIdStore();
   const { isTeamDetailModalOpen, setIsTeamDetailModalOpen } =
     useTeamDetailModalStore();
@@ -67,13 +70,21 @@ const MatchTeamPage = () => {
       >
         <BackPageButton />
         <div className={cn('flex', 'w-full', 'gap-[1.5rem]', 'flex-col')}>
-          <MatchNameContainer gameData={gameData} />
-
+          <div className={cn('flex', 'w-full', 'justify-between')}>
+            <MatchNameContainer gameData={gameData} />
+            <ShowBracketButton onClick={() => setIsBracketModalOpen(true)} />
+          </div>
           <TeamListContainer teams={teamInfoData} />
         </div>
       </div>
       {isTeamDetailModalOpen && (
         <TeamDetailModal onClose={() => setIsTeamDetailModalOpen(false)} />
+      )}
+      {isBracketModalOpen && (
+        <BracketModal
+          onClose={() => setIsBracketModalOpen(false)}
+          _gameId={1}
+        />
       )}
     </div>
   );
