7419c71d9638020dacb20febeb3774d633fd8d12
송재욱 <haensol976520@gmail.com>
2025-04-07 13:59:20 +0900

    feat: /stage 페이지에 점검중 모달 적용

--- src/app/(header)/stage/page.tsx
+++ src/app/(header)/stage/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import useInspectionModalStore from '@/shared/stores/useInspectionModalStore';
+import InspectionModal from '@/shared/ui/InspectionModal';
 import StagePage from '@/views/stage/ui/StagePage';
 
 const page = () => {
-  return <StagePage />;
+  const { isInspectionModalOpen, setIsInspectionModalOpen } =
+    useInspectionModalStore();
+  return (
+    <>
+      <StagePage />
+      {isInspectionModalOpen && (
+        <InspectionModal onClose={() => setIsInspectionModalOpen(false)} />
+      )}
+    </>
+  );
 };
 
 export default page;
