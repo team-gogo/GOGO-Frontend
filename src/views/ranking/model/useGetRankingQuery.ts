d09d5cd10ab91aeae1f368dc3a4bff6483b60a17
gjaegyun <jaegyungim06@gmail.com>
2025-04-08 16:39:06 +0900

    feat: ranking 무한 스크롤 기능 제작

--- src/views/ranking/model/useGetRankingQuery.ts
+++ src/views/ranking/model/useGetRankingQuery.ts
@@ -2,9 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { RankingData } from '@/shared/types/ranking';
 import { getRank } from '../api/getRank';
 
-export const useGetRankingQuery = (stageId: string) => {
+export const useGetRankingQuery = (
+  stageId: string,
+  page: number,
+  size: number,
+) => {
   return useQuery<RankingData, Error>({
-    queryKey: ['ranking', stageId],
-    queryFn: () => getRank(stageId),
+    queryKey: ['ranking', stageId, page, size],
+    queryFn: () => getRank(stageId, page, size),
   });
 };
