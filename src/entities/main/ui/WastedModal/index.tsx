fbd9969b92b3d4a19bfe8dc72013c58909d2e695
송재욱 <haensol976520@gmail.com>
2025-04-10 20:23:46 +0900

    feat: 디자인 반영

--- src/entities/main/ui/WastedModal/index.tsx
+++ src/entities/main/ui/WastedModal/index.tsx
@@ -60,7 +60,7 @@ const WastedModal = ({ onClose, stageId }: WastedModalProps) => {
         'bg-gray-700',
         'laptop:px-[40px]',
         'laptop:py-[36px]',
-        'laptop:max-w-[50rem]',
+        'laptop:max-w-[55rem]',
         'w-full',
         'space-y-24',
         'px-[20px]',
@@ -77,7 +77,7 @@ const WastedModal = ({ onClose, stageId }: WastedModalProps) => {
         ref={contentRef}
         className={cn(
           'w-full',
-          'max-h-[40vh]',
+          'max-h-[45vh]',
           'text-gray-400',
           'overflow-y-auto',
           'flex-1',
@@ -93,21 +93,31 @@ const WastedModal = ({ onClose, stageId }: WastedModalProps) => {
             'text-body2s',
             'mobile:text-body3s',
             'phone:text-body3s',
-            'py-[0.75rem]',
+            'py-[0.5rem]',
           )}
         >
-          &quot;The house always wins.&quot;– 불변의 진리 도박은 언젠가 반드시
-          패배하게 됩니다. 수많은 선택과 예측 속에서 잠깐의 승리는 달콤했을지
-          몰라도, 결과는 늘 같습니다. 당신의 패배는 확률의 귀결이며, 시스템의
-          결과는 늘 같습니다. 당신의 패배는 확률의 귀결이며, 시스템의 당연한
-          승리입니다.
+          &quot;The house always wins.&quot;– <br />
+          불변의 진리
         </p>
         <p
           className={cn(
             'text-body2s',
             'mobile:text-body3s',
             'phone:text-body3s',
-            'py-[0.75rem]',
+            'py-[0.5rem]',
+          )}
+        >
+          도박은 언젠가 반드시 패배하게 됩니다. 수많은 선택과 예측 속에서 잠깐의
+          승리는 달콤했을지 몰라도, 결과는 늘 같습니다. 당신의 패배는 확률의
+          귀결이며, 시스템의 결과는 늘 같습니다. 당신의 패배는 확률의 귀결이며,
+          시스템의 당연한 승리입니다.
+        </p>
+        <p
+          className={cn(
+            'text-body2s',
+            'mobile:text-body3s',
+            'phone:text-body3s',
+            'py-[0.5rem]',
           )}
         >
           도박에서 한두 번 이길 수도 있습니다. 하지만 큰 수의 법칙에 따르면
@@ -120,37 +130,51 @@ const WastedModal = ({ onClose, stageId }: WastedModalProps) => {
             'text-body2s',
             'mobile:text-body3s',
             'phone:text-body3s',
-            'py-[0.75rem]',
+            'py-[0.5rem]',
           )}
         >
           그리고 도박이 남기는 것은 단순한 패배가 아닙니다. 청소년 도박 중독자는
           성인이 되어서도 도박을 끊기 어려우며, 초기의 작은 승리는 더 큰 중독을
           불러옵니다. 결국 빚과 신용 문제, 심지어 범죄로 이어질 수도 있습니다.
-          도박은 재미일 수 있지만, 중독은 현실을 무너뜨립니다. Team. GOGO는
-          청소년 불법 도박 근절 캠페인과 함께, 한 번의 기회로 건강한 리셋을
-          제공합니다.
+          도박은 재미일 수 있지만, 중독은 현실을 무너뜨립니다.
         </p>
         <p
           className={cn(
             'text-body2s',
             'mobile:text-body3s',
             'phone:text-body3s',
-            'py-[0.75rem]',
+            'py-[0.5rem]',
+            'text-white',
+          )}
+        >
+          Team. GOGO는 청소년 불법 도박 근절 캠페인과 함께, 한 번의 기회로
+          건강한 리셋을 제공합니다.
+        </p>
+        <p
+          className={cn(
+            'text-body2s',
+            'mobile:text-body3s',
+            'phone:text-body3s',
+            'py-[0.5rem]',
+            'text-white',
           )}
         >
           2층 홈베이스에 비치되어있는 서약서를 작성하고 하고 제출함에 제출하면
-          한 번의 기회로 포인트를 지원받을 수 있습니다.
+          한 번의 기회로 포인트를 지원받을 수 있습니다. (전체 포인트의 평균,
+          최소 3만, 최대 5만)
         </p>
         <p
           className={cn(
-            'text-body2s',
+            'text-body3s',
             'mobile:text-body3s',
             'phone:text-body3s',
-            'py-[0.75rem]',
+            'py-[0.5rem]',
+            'text-white',
           )}
         >
-          (전체 포인트의 평균, 최소 3만, 최대 5만) 그러나 다시는 같은 실수를
-          반복하지 마세요.
+          <span className="text-body3s font-[800]">
+            그러나 다시는 같은 실수를 반복하지 마세요.
+          </span>
         </p>
       </div>
 
