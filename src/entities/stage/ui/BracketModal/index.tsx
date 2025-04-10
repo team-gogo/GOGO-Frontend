80945550f09eba7e4423ba95a14747d6fbf17586
송재욱 <haensol976520@gmail.com>
2025-04-11 08:07:54 +0900

    feat: 대진표 모달 폭 수정

--- src/entities/stage/ui/BracketModal/index.tsx
+++ src/entities/stage/ui/BracketModal/index.tsx
@@ -137,8 +137,7 @@ const BracketModal = ({ onClose, gameId }: BracketModalProps) => {
         'rounded-lg',
         'bg-gray-700',
         'p-[40px]',
-        'max-w-[62.5rem]',
-
+        'max-w-[70rem]',
         'w-[calc(100%-40px)]',
         'space-y-24',
         'mx-20',
@@ -155,7 +154,7 @@ const BracketModal = ({ onClose, gameId }: BracketModalProps) => {
           <div
             className="relative h-full"
             style={{
-              minWidth: finalStage === 4 ? '900px' : '1200px',
+              minWidth: finalStage === 4 ? '900px' : '1000px',
               width: 'max-content',
             }}
           >
