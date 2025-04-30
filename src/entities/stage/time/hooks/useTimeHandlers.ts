2293fd8622ddc0a03b07d300b1c28e74e1ab3081
송재욱 <haensol976520@gmail.com>
2025-04-30 17:23:53 +0900

    feat: value 상태 변경 핸들러 추가

--- src/entities/stage/time/hooks/useTimeHandlers.ts
+++ src/entities/stage/time/hooks/useTimeHandlers.ts
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { MatchSelection, MatchTeams } from '../types/match';
+import { useState, useEffect } from 'react';
+import { MatchSelection, MatchTeams, SavedMatchData } from '../types/match';
 
 export const useTimeHandlers = (
   selectedMatch: MatchSelection | null,
@@ -13,10 +13,34 @@ export const useTimeHandlers = (
     teamAName: string,
     teamBName: string,
   ) => void,
+  savedMatches: SavedMatchData[],
 ) => {
   const [date, setDate] = useState('');
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
+
+  useEffect(() => {
+    if (selectedMatch) {
+      const savedMatch = savedMatches.find(
+        (match) =>
+          match.round === selectedMatch.round &&
+          match.index === selectedMatch.index,
+      );
+
+      if (savedMatch) {
+        const startDate = new Date(savedMatch.startDate);
+        const endDate = new Date(savedMatch.endDate);
+
+        setDate(startDate.toISOString().split('T')[0]);
+        setStartTime(startDate.toTimeString().split(' ')[0].substring(0, 5));
+        setEndTime(endDate.toTimeString().split(' ')[0].substring(0, 5));
+      } else {
+        setDate('');
+        setStartTime('');
+        setEndTime('');
+      }
+    }
+  }, [selectedMatch, savedMatches]);
 
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDate(e.target.value);
