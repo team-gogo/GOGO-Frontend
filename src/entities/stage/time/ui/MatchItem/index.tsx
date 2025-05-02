e740d5c244cc6cc81086badc8439a4508d2a7217
송재욱 <haensol976520@gmail.com>
2025-05-02 10:47:47 +0900

    feat: matchItem 반응형

--- src/entities/stage/time/ui/MatchItem/index.tsx
+++ src/entities/stage/time/ui/MatchItem/index.tsx
@@ -21,13 +21,13 @@ const MatchItem = ({
 }: MatchItemProps) => {
   return (
     <div
-      className={`flex min-h-[60px] w-full items-center rounded-lg ${solved ? 'bg-gray-600' : 'bg-main-600'} ${selected ? 'outline-3 outline outline-main-600' : ''} cursor-pointer text-white phone:p-4 mobile:p-6 pad:p-8`}
+      className={`flex min-h-[60px] w-full items-center rounded-lg ${solved ? 'bg-gray-600' : 'bg-main-600'} ${selected ? 'outline-3 outline outline-main-600' : ''} cursor-pointer text-white phone:p-4 mobile:p-6 pad:p-8 desktop:min-w-[300px] desktop:p-6`}
       onClick={onClick}
     >
-      <div className="font-bold phone:mr-2 phone:text-body2s mobile:mr-3 mobile:text-body1s pad:mr-4 pad:text-h4s">
+      <div className="font-bold phone:mr-2 phone:text-body2s mobile:mr-3 mobile:text-body1s pad:mr-4 pad:text-h4s desktop:mr-4">
         {index}
       </div>
-      <div className="flex flex-1 items-center justify-center">
+      <div className="flex flex-1 items-center justify-center desktop:gap-4">
         <div className="pad:text-16 max-w-[35%] truncate font-medium phone:text-caption1s mobile:text-body2s">
           {teamAName}
         </div>
